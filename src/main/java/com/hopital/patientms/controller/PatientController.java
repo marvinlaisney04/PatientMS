e93e8dd5149fd8ca8f6f33d21831fe4807949857
marvinlaisney04 <laisneymarvin@gmail.com>
2022-05-17 17:58:39 +0200

    allow cors

--- src/main/java/com/hopital/patientms/controller/PatientController.java
+++ src/main/java/com/hopital/patientms/controller/PatientController.java
@@ -7,12 +7,14 @@ import com.hopital.patientms.model.Patient;
 import com.hopital.patientms.repository.PatientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api")
 public class PatientController {
 
